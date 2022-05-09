e579eee8088825b6da957a1b8369e207bc1418a3
L-tech <legendabrahamonoja@gmail.com>
2022-05-09 08:53:18 +0100

    Contract update for added functionalities

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,6 @@
 import { Charity, listedCharities } from './model';
-import { ContractPromiseBatch, context } from 'near-sdk-as';
+import { ContractPromiseBatch, context, u128 } from 'near-sdk-as';
+// import { compareImpl } from 'assemblyscript/std/assembly/util/string';
 
 export function setCharity(charity: Charity): void {
     let storedCharity = listedCharities.get(charity.id);
@@ -41,3 +42,62 @@ export function donateToCharity(charityId: string): void {
     charity.increaseDonation(context.attachedDeposit);
     listedCharities.set(charity.id, charity);
 }
+
+// donate 1 Near to all charity projects
+export function donateToAll(): void {
+    const charities = getCharities().filter(charity => !charity.completed);
+    const total = (1000000000000000000000000 * charities.length).toString();
+    if(context.attachedDeposit < u128.fromString(total)) {
+        throw new Error("attached deposit should be greater than " + total);
+    }
+    charities.forEach(charity => {
+
+        ContractPromiseBatch.create(charity.owner).transfer(u128.fromString("1000000000000000000000000"));
+
+        if(!charity.donors.includes(context.sender)) {
+            charity.donors.push(context.sender);
+        }
+        if("1000000000000000000000000" > charity.highest.toString()) { 
+            charity.highest = u128.fromString("1000000000000000000000000")
+        }
+        charity.increaseDonation(u128.fromString("1000000000000000000000000"));
+        listedCharities.set(charity.id, charity);
+    });
+
+}
+
+// transfer the ownership of a charity project to a new owner
+export function transferCharityOwnership(charityId: string, newOwner: string): void {
+    const charity = getCharity(charityId);
+    if (charity == null) {
+        throw new Error("charity not found");
+    }
+    if(charity.owner != context.sender) {
+        throw new Error("you are not the owner of this charity project");
+    }
+    charity.owner = newOwner;
+    listedCharities.set(charity.id, charity);
+}
+
+// get all uncompleted charity projects
+
+export function getUncompletedCharities(): Charity[] {
+    const charities = getCharities().filter(charity => !charity.completed);
+    return charities;
+}
+
+
+// delete a charity
+export function deleteCharity(charityId: string): void {
+    const charity = getCharity(charityId);
+    if (charity == null) {
+        throw new Error("charity not found");
+    }
+    if(charity.owner != context.sender) {
+        throw new Error("you are not the owner of this charity project");
+    }
+    listedCharities.delete(charity.id);
+}
+
+
+
